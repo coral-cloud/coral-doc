b7635a0166278749e594559a09e044e5a630674e
wuhao <wuhaocn@126.com>
2020-06-17 22:58:04 +0800

    docker run1

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker stop site-doc
 docker rm site-doc
-docker run -it  --privileged=true -p 8080:8080 --name site-doc site-doc:20200617224416
-#docker run -d  --privileged=true -p 8093:8093 --name site-doc 10.10.208.193:5000/site-doc:20190531145408
+docker run -it  --privileged=true -p 8080:8080 --name site-doc site-doc:20200617224720
+#docker run -it  --privileged=true -p 8093:8093 --name site-doc 10.10.208.193:5000/site-doc:20200617224720
 docker logs -f site-doc
