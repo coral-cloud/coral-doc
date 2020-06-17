d627615f4b0a07cf4149be1052aab8ef3a97edb9
wuhao <wuhaocn@126.com>
2020-06-17 22:05:01 +0800

    init

---
+++ docker-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+version=`date +%Y%m%d%H%M%S`;
+echo "build site-doc version $version"
+npm install
+rm -rf ./docker/docs
+rm -rf ./docker/node_modules
+cp -rf ./docs  ./docker/docs
+cp -rf ./node_modules  ./docker/node_modules
+cp -rf ./*.json  ./docker/
+cp -rf ./*.md  ./docker/
+#docker rm `docker ps -a -q`
+docker rmi --force `docker images | grep site-doc | awk '{print $3}'`
+docker rmi --force `docker images | grep none | awk '{print $3}'`
+docker build -t site-doc:$version ./docker
+#docker tag site-doc:$version 10.10.208.193:5000/site-doc:$version
+#
+#docker push 10.10.208.193:5000/site-doc:$version
+echo "build booth sucess 10.10.208.193:5000/site-doc:$version"
